f1cbeba1fc29e4d1a9e109998950764848bc56ec
MegaFason <karol.zorniak@gmail.com>
2025-04-01 21:47:21 +0200

    fix: update for async params in Next 15

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,7 +3,8 @@ import dynamic from "next/dynamic";
 // klientowy komponent – dynamiczny import (bez SSR)
 const ProductCard = dynamic(() => import("@/components/ProductCard"), { ssr: false });
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   return (
     <main className="p-4">
       <ProductCard productId={params.id} />
